d6ce18d1885fccdee26b69844f4ee2993e091ed7
eckrin <bsh51515@naver.com>
2024-08-06 16:34:44 +0900

    test: 비동기 처리 테스트코드 수정

--- src/test/java/com/softeer/podoarrival/event/service/ArrivalEventServiceTest.java
+++ src/test/java/com/softeer/podoarrival/event/service/ArrivalEventServiceTest.java
@@ -13,9 +13,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -54,14 +52,16 @@ class ArrivalEventServiceTest {
             long userId = i;
             executorService.submit(() -> {
                 try {
-                    ArrivalApplicationResponseDto res= arrivalEventService.applyEvent(
+                    CompletableFuture<ArrivalApplicationResponseDto> futureResponse = arrivalEventService.applyEvent(
                             new AuthInfo(
                                     "teat" + userId,
                                     "010-1234-5678-" + userId,
                                     Role.ROLE_USER
                             )
                     );
-                    if(res.getResponse().equals("선착순 응모에 성공했습니다.")) count.getAndIncrement();
+                    if(futureResponse.get().getResponse().equals("선착순 응모에 성공했습니다.")) count.getAndIncrement();
+                } catch (ExecutionException | InterruptedException e) {
+                    throw new RuntimeException(e);
                 } finally {
                     countDownLatch.countDown();
                 }
