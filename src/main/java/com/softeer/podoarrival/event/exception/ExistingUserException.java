4d37b5a64311e9d2586b2d5156982fadd2e769fc
eckrin <bsh51515@naver.com>
2024-08-06 11:24:53 +0900

    fix: Exception에 빠진 Getter 추가

--- src/main/java/com/softeer/podoarrival/event/exception/ExistingUserException.java
+++ src/main/java/com/softeer/podoarrival/event/exception/ExistingUserException.java
@@ -1,8 +1,10 @@
 package com.softeer.podoarrival.event.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class ExistingUserException extends RuntimeException {
     private String message;
 }
