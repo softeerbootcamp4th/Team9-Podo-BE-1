b0cf6eaa0640aca8b4fb924d96a73b5f24e879cc
eckrin <bsh51515@naver.com>
2024-08-06 11:30:31 +0900

    fix: 1차 검증 방법을 Redis Key 대신 check flag를 사용하도록 수정
    - 로컬 성능테스트 결과에는 크게 영향이 없었음
    - 네트워크 i/o cost 절약 가능

--- src/main/java/com/softeer/podoarrival/event/service/ArrivalEventService.java
+++ src/main/java/com/softeer/podoarrival/event/service/ArrivalEventService.java
@@ -7,15 +7,16 @@ import com.softeer.podoarrival.user.model.entity.ArrivalUser;
 import com.softeer.podoarrival.user.model.entity.Role;
 import com.softeer.podoarrival.user.repository.ArrivalUserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.BatchResult;
 import org.redisson.api.RBatch;
-import org.redisson.api.RBucket;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ArrivalEventService {
@@ -23,20 +24,19 @@ public class ArrivalEventService {
     private final RedissonClient redissonClient;
     private final ArrivalUserRepository arrivalUserRepository;
 
-    private final String FINISHED = "finished";
     private final String ARRIVAL_SET = "arrivalset";
-    private final String CHECK = "check";
+    private boolean CHECK = false;
 
     private final int MAX_ARRIVAL = 100;
 
     @Transactional
     public ArrivalApplicationResponseDto applyEvent(AuthInfo authInfo){
         LocalDate now = LocalDate.now();
-        RBucket<String> check = redissonClient.getBucket(now + CHECK);
 
-        if(check.get() != null && check.get().equals(FINISHED)){
-            return new ArrivalApplicationResponseDto("선착순 응모에 실패했습니다.");
+        if(CHECK){
+            return new ArrivalApplicationResponseDto("선착순 응모에 실패했습니다.", -1);
         }
+        log.info("전화번호 = {}", authInfo.getPhoneNum());
 
         RBatch batch = redissonClient.createBatch();
         batch.getSet(now + ARRIVAL_SET).addAsync(authInfo.getPhoneNum());
@@ -45,22 +45,25 @@ public class ArrivalEventService {
 
         //첫번째 응답
         if(!(boolean) res.getResponses().get(0)){
+            log.info("전화번호-중복 = {}", authInfo.getPhoneNum());
             throw new ExistingUserException("이미 응모한 전화번호입니다.");
         }
 
-        if((int) res.getResponses().get(1) <= MAX_ARRIVAL){
+        int grade = (int) res.getResponses().get(1);
+        // 선착순 순위에 들었다면
+        if(grade <= MAX_ARRIVAL){
             arrivalUserRepository.save(
                     ArrivalUser.builder()
                             .name(authInfo.getName())
                             .phoneNum(authInfo.getPhoneNum())
                             .role(Role.ROLE_USER)
-                            .arrivalRank((int) res.getResponses().get(1))
+                            .arrivalRank(grade)
                             .build()
             );
-            return new ArrivalApplicationResponseDto("선착순 응모에 성공했습니다.");
-        }else{
-            check.set(FINISHED);
-            return new ArrivalApplicationResponseDto("선착순 응모에 실패했습니다.");
+            return new ArrivalApplicationResponseDto("선착순 응모에 성공했습니다.", grade);
+        } else {
+            CHECK = true;
+            return new ArrivalApplicationResponseDto("선착순 응모에 실패했습니다.", -1);
         }
     }
 
