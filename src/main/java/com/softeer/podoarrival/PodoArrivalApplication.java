7d7b05cd0dfbda27dbd74283f7eca4c5a2d81515
luna156 <v010309@naver.com>
2024-08-07 17:26:44 +0900

    feat: cors에러 해결
    - allowed origin에 프론트 서버 url 필요

--- src/main/java/com/softeer/podoarrival/PodoArrivalApplication.java
+++ src/main/java/com/softeer/podoarrival/PodoArrivalApplication.java
@@ -2,9 +2,12 @@ package com.softeer.podoarrival;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableAsync
 @EnableScheduling
@@ -12,8 +15,19 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class PodoArrivalApplication {
 
+    private static final String allowedOrigin = "http://localhost:8000";
+
     public static void main(String[] args) {
         SpringApplication.run(PodoArrivalApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(allowedOrigin);
+            }
+        };
+    }
 }
