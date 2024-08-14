2c315ab9d072df02483504af74e87d02c6c61067
eckrin <bsh51515@naver.com>
2024-08-14 16:20:05 +0900

    fix: 이벤트 당첨자 수 스케줄링 로직 수정
    - 이벤트 당첨정보에서 reward 개수를 모두 합하도록 수정

--- src/main/java/com/softeer/podoarrival/event/repository/EventRewardRepository.java
+++ src/main/java/com/softeer/podoarrival/event/repository/EventRewardRepository.java
@@ -4,6 +4,9 @@ import com.softeer.podoarrival.event.model.entity.Event;
 import com.softeer.podoarrival.event.model.entity.EventReward;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EventRewardRepository extends JpaRepository<EventReward, Long> {
 	int countByEvent(Event event);
+    List<EventReward> findAllByEvent(Event event);
 }
