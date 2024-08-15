f278bdca16e91038f65d3bdaad333358fd678405
eckrin <bsh51515@naver.com>
2024-08-15 18:04:10 +0900

    test: 스케줄링 로직 변경(2c315ab9)에 따른 테스트 로직 수정

--- src/test/java/com/softeer/podoarrival/unit/base/ArrivalEventMaxCountSchedulerBase.java
+++ src/test/java/com/softeer/podoarrival/unit/base/ArrivalEventMaxCountSchedulerBase.java
@@ -1,5 +1,7 @@
 package com.softeer.podoarrival.unit.base;
 
+import com.softeer.podoarrival.event.model.entity.Event;
+import com.softeer.podoarrival.event.model.entity.EventReward;
 import com.softeer.podoarrival.event.repository.EventRepository;
 import com.softeer.podoarrival.event.repository.EventRewardRepository;
 import com.softeer.podoarrival.event.repository.EventTypeRepository;
@@ -10,6 +12,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
 
 @ExtendWith(MockitoExtension.class)
 public class ArrivalEventMaxCountSchedulerBase {
@@ -26,8 +30,31 @@ public class ArrivalEventMaxCountSchedulerBase {
     @InjectMocks
     protected ArrivalEventMaxArrivalScheduler arrivalEventMaxArrivalScheduler;
 
+    protected Event eventSample;
+    protected EventReward eventReward1;
+    protected EventReward eventReward2;
+
     @BeforeEach
     public void setUp() {
+        eventSample = Event.builder()
+                .id(1L)
+                .title("셀토스 선착순 이벤트")
+                .description("The 2025 셀토스 출시 기념 선착순 이벤트")
+                .startAt(LocalDateTime.now())
+                .endAt(LocalDateTime.now().plusDays(5))
+                .build();
+
+        eventReward1 = EventReward.builder()
+                .id(1L)
+                .event(eventSample)
+                .numWinners(10)
+                .build();
+
+        eventReward2 = EventReward.builder()
+                .id(2L)
+                .event(eventSample)
+                .numWinners(50)
+                .build();
     }
 
     @AfterEach
