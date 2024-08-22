cb213200a21a43ad413222d3fc5fa5ddea2e3330
luna156 <v010309@naver.com>
2024-08-22 11:03:25 +0900

    fix: cors 에러 해결
    - http://localhost:8000, https://www.hyundaiseltos.site 추가

--- src/main/java/com/softeer/podoarrival/PodoArrivalApplication.java
+++ src/main/java/com/softeer/podoarrival/PodoArrivalApplication.java
@@ -2,9 +2,12 @@ package com.softeer.podoarrival;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableAsync
 @EnableScheduling
@@ -16,4 +19,16 @@ public class PodoArrivalApplication {
         SpringApplication.run(PodoArrivalApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8000", "https://www.hyundaiseltos.site")  // 허용할 도메인
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메소드
+                        .allowedHeaders("*"); // 허용할 헤더
+            }
+        };
+    }
 }
