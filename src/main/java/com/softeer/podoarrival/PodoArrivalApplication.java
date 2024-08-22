87d0bf0f442c79cb12a4d79d59324798cfd5a099
luna156 <v010309@naver.com>
2024-08-22 16:08:25 +0900

    feat: 사설 keystore로 https 연결 구현
    - 8081포트에서 https 연결 허용

--- src/main/java/com/softeer/podoarrival/PodoArrivalApplication.java
+++ src/main/java/com/softeer/podoarrival/PodoArrivalApplication.java
@@ -1,7 +1,12 @@
 package com.softeer.podoarrival;
 
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -30,5 +35,22 @@ public class PodoArrivalApplication {
                         .allowedHeaders("*"); // 허용할 헤더
             }
         };
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        // Enable SSL Trafic
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        return tomcat;
     }
 }
